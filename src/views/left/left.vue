1d7f496553ac113684f3cb41b14db733654246b8
Yzhiliang <yin496983120@163.com>
2019-09-21 16:52:40 +0800

    测试分支

---
+++ src/views/left/left.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>
+          5645456564
+      </h1>
+      <ul>
+          <li>sad</li>
+          <li>sad</li>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
